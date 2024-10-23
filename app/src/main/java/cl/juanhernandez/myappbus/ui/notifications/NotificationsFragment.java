50b30ce4ce8a8727ccf33394205b906c801eb091
juanHernandezGlz <juancrishernandezg@gmail.com>
2024-10-23 18:01:31 -0300

    Configuration in the "fragment_login"

--- app/src/main/java/cl/juanhernandez/myappbus/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/cl/juanhernandez/myappbus/ui/notifications/NotificationsFragment.java
@@ -1,15 +1,22 @@
 package cl.juanhernandez.myappbus.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import cl.juanhernandez.myappbus.Login;
+import cl.juanhernandez.myappbus.R;
 import cl.juanhernandez.myappbus.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
@@ -18,7 +25,7 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
+        /*NotificationsViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
@@ -26,7 +33,39 @@ public class NotificationsFragment extends Fragment {
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return root; */
+
+        View rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        Button cerrarSesion = rootView.findViewById(R.id.button_logout);
+
+        TextView textUserName = rootView.findViewById(R.id.text_username);
+        TextView textEmail = rootView.findViewById(R.id.text_email);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(currentUser != null){
+            String email = currentUser.getEmail();
+            String username = email.substring(0, email.indexOf('@'));
+
+            username = username.substring(0,1).toUpperCase() + username.substring(1);
+
+            textUserName.setText(username);
+
+            textEmail.setText(email);
+        }
+
+        //Configuración el listener del botón cerrar sesión
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), Login.class));
+                getActivity().finish();
+            }
+        });
+
+        return rootView;
     }
 
     @Override
